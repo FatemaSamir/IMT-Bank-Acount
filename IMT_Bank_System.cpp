bcb4a54713eaa039aab6e334f3c5eea5d14e2f01
FatemaSamir <201627220@std.sci.cu.edu.eg>
2022-05-10 19:09:12 +0200

    Revert Changes

--- IMT_Bank_System.cpp
+++ IMT_Bank_System.cpp
@@ -74,11 +74,6 @@ void init_vew_window(){
 void Exit_System(){
     exit(0);
 }
-
-
-
-
-
 // Creat Acount class
 class Creat_Bank_Account{
 //  information about user account 
@@ -89,19 +84,19 @@ class Creat_Bank_Account{
         string Guardian;
         int NationalID;
         int age;
-        int BankAcountID;
+        int BankAccountID;
         int GuardianID;
         int Balance;
-        int passowrd; // generate random
+        int password; // generate random
 
     public:
 
     // define conatractor 
     Creat_Bank_Account();
     Creat_Bank_Account(string FullName, string FullAdress, string Account_Status, string Guardian );
+    
 
     // put getter and setter for each attribute 
-    int get_Bank_Account_ID();
 
     // define methods that can user do 
     void init_Client_Window();  // show main_menu
@@ -109,9 +104,33 @@ class Creat_Bank_Account{
     void Get_Cash(float cash_amount);
     void  Deposit_in_Account(float cash_amount);
     void Return_to_main_menu();
+    string set_FullName(string FN);
+    string set_FullAdress(string FA);
+    string set_Account_status(string status);
+    string set_Guardian(string G);
+    int set_NationalID(int NID);
+    int set_Age(int agee);
+    int set_bankAccountID(int BID);
+    int set_GuardianID(int GID);
+    int set_Balance(int b);
+    int set_password(int pass);
+    string get_FullName();
+    string get_FullAdress();
+    string get_Account_status();
+    string get_Guardian();
+    int get_NationalID();
+    int get_Age();
+    int get_bankAccountID();
+    int get_GuardianID();
+    int get_Balance();
+    int get_password();
+
+
+
 
 
 };
+
 // implement methods
 
 Creat_Bank_Account::Creat_Bank_Account(){
@@ -123,9 +142,70 @@ Creat_Bank_Account::Creat_Bank_Account(string FullName, string FullAdress, strin
 }
 
 // put getter and setter for each attribute 
-int Creat_Bank_Account::get_Bank_Account_ID(){
-    return BankAcountID;
-}
+    string Creat_Bank_Account:: set_FullName(string FN){
+        FullName = FN;
+    }
+    string Creat_Bank_Account:: set_FullAdress(string FA){
+        FullAdress = FA;    
+    }
+    string Creat_Bank_Account::set_Account_status(string status){
+        Account_Status = status;
+    }
+    string Creat_Bank_Account:: set_Guardian(string G){
+        Guardian = G;
+    }
+    int Creat_Bank_Account::set_NationalID(int NID){
+        NationalID = NID;
+
+    }
+    int Creat_Bank_Account::set_Age(int agee){
+        age = agee;
+    }
+    int Creat_Bank_Account::set_bankAccountID(int BID){
+        BankAccountID = BID;
+    }
+    int Creat_Bank_Account::set_GuardianID(int GID){
+    GuardianID = GID;
+    }
+    int Creat_Bank_Account::set_Balance(int b){
+        Balance = b;
+
+    }
+    int Creat_Bank_Account::set_password(int pass){
+    password = pass;
+    }
+    string Creat_Bank_Account::get_FullName(){
+        return FullName;
+    }
+
+    string Creat_Bank_Account:: get_FullAdress(){
+        return FullAdress;
+    }
+    string Creat_Bank_Account:: get_Account_status(){
+        return Account_Status;
+    }
+    string Creat_Bank_Account:: get_Guardian(){
+        return Guardian;
+    }
+    int Creat_Bank_Account:: get_NationalID(){
+        return NationalID;
+    }
+    int Creat_Bank_Account:: get_Age(){
+        return age; 
+    }
+    int Creat_Bank_Account:: get_bankAccountID(){
+        return BankAccountID;
+    }
+    int Creat_Bank_Account:: get_GuardianID(){
+        return GuardianID;
+    }
+    int Creat_Bank_Account:: get_Balance(){
+    return Balance;
+    }
+    int Creat_Bank_Account:: get_password(){
+        return password;
+    }
+
 
 // define methods that can user do 
 void Creat_Bank_Account::init_Client_Window(){
