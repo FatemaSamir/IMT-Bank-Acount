e6c3459a82c0c40dc3c1201ff6437b2de9467998
FatemaSamir <201627220@std.sci.cu.edu.eg>
2022-05-11 02:10:34 +0200

    solve All errors

--- IMT_Bank_System.cpp
+++ IMT_Bank_System.cpp
@@ -2,21 +2,24 @@
 // imports 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
-
 
 // Main class
 int main(){
 
     int window ;
+    string admin_name;
+    int Admin_pass;
 
-    cout<< "choose"<<"\n "<<"1- Admin Window."<<"\n 2- Client Window.";
+    cout<< "choose"<<"\n "<<"1- Admin Window."<<"\n 2- Client Window. \n";
     // if choose 1 view Admin Window
     // if choose 2 view Client Window
     cin>> window ;
     
     if (window == 1 ){
         cout<<"Enter Admin Username and Password ";
+        scanf("%s %d",&admin_name, &Admin_pass);
     }
 
     else if (window == 2 ) {
@@ -30,91 +33,6 @@ int main(){
 
 return 0;
 }
-
-
-// Admin calss
-class  Admin_Window{
-
-    public:
-        void init_vew_window();
-        void creat_Account();
-        void change_statuse();
-        void open_existing_account();
-        void Exit_System();
-
-};
-
-void init_vew_window(){
-
-    int mode;
-        printf("1- Creat Acount \n2- Open Existing Acount \n3- Exit System");
-    cin>> mode ;
-
-
-    if (mode == 1){
-        // creat new acount 
-
-    }
-
-    else if (mode == 2){
-        // Open Existing Acount 
-        
-    }
-    else if (mode == 3){
-        // Exit System
-    // Exit_System();
-
-    }
-
-    else
-        cout<<"Sorry, Please choose 1 or 2 or 3 ";
-
-}
-
-void Exit_System(){
-    exit(0);
-}
-
-void creat_Account(){
-
-    string FullName;
-    string FullAdress;
-    string Account_Status;
-    string Guardian;
-    int NationalID;
-    int age;
-    int Guardian_id;
-    int Balance;
-    bool gradian = false;
-    cout << "- Full Name : ";
-    cin >> FullName;
-    cout <<"- Full Address : ";
-    cin >> FullAdress;
-    cout << "- Age : ";
-    cin >> age;
-    if (age < 21){
-        cout << "- Guardian National ID : ";
-        cin >> Guardian_id;
-        cout << "- Guardian Name : ";
-        cin >> Guardian;
-        gradian = true;
-    }
-    cout << "- National ID : ";
-    cin >> NationalID;
-    cout << "- Balance : ";
-    cin >> Balance;
-
-//  Creat Account with Guardian_id 
-if (gradian)
-        Creat_Bank_Account account = new Creat_Bank_Account(FullName, FullAdress, NationalID, age , Guardian_id, Guardian, Balance);
-
-//  Creat Account without Guardian_id 
-else 
-    Creat_Bank_Account account = new Creat_Bank_Account(FullName, FullAdress, age , Balance);
-}
-
-
-
 // Creat Acount class
 class Creat_Bank_Account{
 //  information about user account 
@@ -124,7 +42,7 @@ class Creat_Bank_Account{
         string Account_Status; // by defult active 
         string Guardian;
         int NationalID;
-        int Age;
+        int age;
         int BankAccountID;
         int GuardianID;
         int Balance;
@@ -134,33 +52,8 @@ class Creat_Bank_Account{
 
     // define conatractor 
     Creat_Bank_Account(){
-        
+            Account_Status = "Active";
     }
-
-    Creat_Bank_Account(string Name, string Adress, int nationalID,  int age, int balance){
-
-        this->FullName = Name;
-        FullAdress = Adress;
-        NationalID = nationalID;
-        Age = age;
-        Balance = balance;
-        Account_Status = "Active";
-
-    }
-    
-    Creat_Bank_Account(string Name, string Adress,  int nationalID, int age, int guardian_id, string guardian, int balance){
-        
-        this->FullName = Name;
-        FullAdress = Adress;
-        NationalID = nationalID;
-        Age = age;
-        Balance = balance;
-        Account_Status = "Active";
-        GuardianID = guardian_id;
-        Guardian = guardian;
-
-    }
-    
 
     // put getter and setter for each attribute 
 
@@ -169,17 +62,17 @@ class Creat_Bank_Account{
     void Change_Account_Password();
     void Get_Cash(float cash_amount);
     void  Deposit_in_Account(float cash_amount);
+    void set_Account_status(string status);
     void Return_to_main_menu();
-    string set_FullName(string FN);
-    string set_FullAdress(string FA);
-    string set_Account_status(string status);
-    string set_Guardian(string G);
+    void set_Guardian(string G);
+    int set_FullName(string FN);
+    int set_FullAdress(string FA);
     int set_NationalID(int NID);
-    int set_Age(int agee);
+    int set_Age(int age);
     int set_bankAccountID(int BID);
     int set_GuardianID(int GID);
     int set_Balance(int b);
-    int set_password(int pass);
+    int set_password();
     string get_FullName();
     string get_FullAdress();
     string get_Account_status();
@@ -197,41 +90,80 @@ class Creat_Bank_Account{
 
 };
 
+
+// define list node 
+typedef struct listNode {
+    Creat_Bank_Account account;
+    struct listNode* next;
+}Node;
+
+// define list based on node 
+
+typedef struct listbasedlinked{
+    Node* head;
+    int size;
+}List;
+
+void ListInit(List* pl);
+void AddNode(List* pl, int pos, Creat_Bank_Account account);
+int AcountIsExist(List* pl,int Bank_Account_ID);
+Node* FindAcount(List* pl,int Bank_Account_ID);
+
+// Admin calss
+class  Admin_Window{
+
+    public:
+        void init_vew_window();
+        void creat_Account(List* L);
+        void change_statuse();
+        void open_existing_account();
+        void Exit_System();
+
+};
+
+
 // implement methods
 
 
 // put getter and setter for each attribute 
-    string Creat_Bank_Account:: set_FullName(string FN){
+    int Creat_Bank_Account::set_FullName(string FN){
         FullName = FN;
+        return 1;
     }
-    string Creat_Bank_Account:: set_FullAdress(string FA){
-        FullAdress = FA;    
+    int Creat_Bank_Account::set_FullAdress(string FA){
+        FullAdress = FA; 
+        return 1;   
     }
-    string Creat_Bank_Account::set_Account_status(string status){
+    void Creat_Bank_Account::set_Account_status(string status){
         Account_Status = status;
     }
-    string Creat_Bank_Account:: set_Guardian(string G){
+    void Creat_Bank_Account:: set_Guardian(string G){
         Guardian = G;
     }
     int Creat_Bank_Account::set_NationalID(int NID){
         NationalID = NID;
+        return 1;
 
     }
-    int Creat_Bank_Account::set_Age(int agee){
-        age = agee;
+    int Creat_Bank_Account::set_Age(int age){
+        age = age;
+        return 1;
     }
     int Creat_Bank_Account::set_bankAccountID(int BID){
-        BankAccountID = BID;
+        BankAccountID = 1000000000+BID;
+        return 1;
     }
     int Creat_Bank_Account::set_GuardianID(int GID){
     GuardianID = GID;
+    return 1;
     }
     int Creat_Bank_Account::set_Balance(int b){
         Balance = b;
+        return 1;
 
     }
-    int Creat_Bank_Account::set_password(int pass){
-    password = pass;
+    int Creat_Bank_Account::set_password(){
+    password  = rand();;
     }
     string Creat_Bank_Account::get_FullName(){
         return FullName;
@@ -287,22 +219,161 @@ void Creat_Bank_Account::Return_to_main_menu(){
 
 
 
-// define list node 
-typedef struct listNode {
+
+void init_vew_window(){
+
+    int mode;
+        printf("1- Creat Acount \n2- Open Existing Acount \n3- Exit System");
+    cin>> mode ;
+
+
+    if (mode == 1){
+        // creat new acount 
+
+    }
+
+    else if (mode == 2){
+        // Open Existing Acount 
+        
+    }
+    else if (mode == 3){
+        // Exit System
+    // Exit_System();
+
+    }
+
+    else
+        cout<<"Sorry, Please choose 1 or 2 or 3 ";
+
+}
+
+void Exit_System(){
+    exit(0);
+}
+
+void change_statuse(){
+    int stat;
+    cout<<"1- Set Account to Active State \n2- Set Account to Restricted State \n3- Set Account to Closed";
+    cin>>stat;
+    switch (stat)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+        printf("Invalid Choice please try again\n");
+            break;
+        }
+
+}
+
+void open_existing_account(List* L){
+    int exist =-1;
+    int ID;
+    Creat_Bank_Account Acc;
+    
+    while (exist==-1)
+    {
+        cout << "Enter Client Bank Account ID ";
+        cin >> ID;
+        // exist = AcountIsExist(L,ID);
+        Acc = FindAcount(L, ID)->account;
+        if(Acc.get_bankAccountID()==ID){
+        // go to client Window
+        printf("client Window Hellow .....\n");
+        }
+        else
+            printf("Invalid Bank Account ID please try again\n");
+    }
+    
+
+}
+
+void creat_Account(List* L){
+
+    string FullName;
+    string FullAdress;
+    string Account_Status;
+    string Guardian;
+    int NationalID;
+    int age;
+    int Guardian_id;
+    int Balance;
+    bool gradian = true;
+    int condi = -1;
+
+//  creat object Account
     Creat_Bank_Account account;
-    struct listNode* next;
-}Node;
+    account.set_bankAccountID(L->size);
+    account.set_password();
 
-// define list based on node 
+    
+    while (condi == -1)
+    {
+        cout << "- Full Name : ";
+        cin >> FullName;
+        condi = account.set_FullName(FullName);
+    }
 
-typedef struct listbasedlinked{
-    Node* head;
-    int size;
-}List;
+    while (condi == -1)
+    {
+        cout <<"- Full Address : ";
+        cin >> FullAdress;
+        condi = account.set_FullAdress(FullAdress);
+    } 
+    
+    while (condi == -1)
+    {
+        cout << "- Age : ";
+        cin >> age;
+        condi = account.set_Age(age);
+    } 
 
-void ListInit(List* pl);
-void AddNode(List* pl, int pos, Creat_Bank_Account account);
-int AcountIsExist(List* pl,int Bank_Account_ID);
+    if (age < 21){
+        while (condi == -1)
+        { 
+                cout << "- Guardian National ID : ";
+                cin >> Guardian_id;
+                condi = account.set_GuardianID(Guardian_id);
+                cout << "- Guardian Name : ";
+                cin >> Guardian;
+                account.set_Guardian(Guardian);
+            }
+        
+    } 
+
+    while (condi == -1)
+        {
+            cout << "- National ID : ";
+            cin >> NationalID;
+            condi = account.set_NationalID(NationalID);
+        } 
+
+    while (condi == -1)
+        {
+            
+        cout << "- Balance : ";
+        cin >> Balance;
+            condi = account.set_Balance(Balance);
+        } 
+
+// AddNode (Account) To list
+AddNode(L,(L)->size, account);
+
+// //  Creat Account with Guardian_id 
+// if (gradian)
+//         Creat_Bank_Account account = new Creat_Bank_Account(FullName, FullAdress, NationalID, age , Guardian_id, Guardian, Balance);
+
+// //  Creat Account without Guardian_id 
+// else 
+//     Creat_Bank_Account account = new Creat_Bank_Account(FullName, FullAdress, age , Balance);
+
+}
+
+
 
 void ListInit(List* pl){
     pl->size = 0;
@@ -333,15 +404,25 @@ void AddNode(List* pl, int pos, Creat_Bank_Account copyaccount){
 }
 
 int AcountIsExist(List* pl, int Bank_Account_ID){
-    int index = 0;
     Node* temp = pl->head;
         while(temp){
             if(temp->account.get_bankAccountID()==Bank_Account_ID)
-                return index;
+                break;
 
             temp = temp->next;
-            index++;
         }
 
-        return -1;
+    return temp->account.get_bankAccountID();
+}
+
+Node* FindAcount(List* pl, int Bank_Account_ID){
+    Node* temp = pl->head;
+        while(temp){
+            if(temp->account.get_bankAccountID()==Bank_Account_ID)
+                break;
+
+            temp = temp->next;
+        }
+
+    return temp;
 }
